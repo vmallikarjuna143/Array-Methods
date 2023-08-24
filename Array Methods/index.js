7749f161feb0d4ab6a90436acc35cc5e6a825820
vmallikarjuna143 <125353979+vmallikarjuna143@users.noreply.github.com>
2023-08-24 17:08:49 +0530

    2 files modified, 4 added, 2 deleted

---
+++ Array Methods/index.js
@@ -0,0 +1,40 @@
+// Write Javascript code!
+// const appDiv = document.getElementById('app');
+// appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+// Join
+const num = ['1', '2', '3', '4'];
+console.log(num.join('*'));
+
+// push
+const nam = ['arjun', 'krishna', 'Malli'];
+console.log(nam.push('kk', 'test'));
+
+const name1 = [1, 2, 3, 4, 5, 6];
+console.log(name1.push(7, 8));
+
+// pop
+const num1 = ['aa', 'bb', 'cc', 'dd'];
+console.log(num1.pop());
+
+const num2 = [1, 2, 3, 4, 5, 6];
+console.log(num2.pop());
+
+// shift
+// unshift
+// for
+// foreach
+// filter
+// splice
+// slice
+// every
+// some
+// reduce
+// map
+
+// includes
+const ar = [1, 2, 3, 4, 5, 6, 7];
+console.log(ar.includes(4));
+
+// find
+// findIndex
